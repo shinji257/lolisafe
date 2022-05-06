5d3ef6e566f028d4708f733ba2c7476923b09276
Bobby <bobby@fiery.me>
2022-05-07 01:36:38 +0700

    feat: perms.group() to get user's group name

--- controllers/permissionController.js
+++ controllers/permissionController.js
@@ -6,7 +6,21 @@ self.permissions = Object.freeze({
   admin: 80, // Manage users (disable accounts) & create moderators
   superadmin: 100 // Create admins
   // Groups will inherit permissions from groups which have lower value
+  // You should NOT have multiple groups with exact same values
 })
+
+self.keys = Object.freeze(Object.keys(self.permissions))
+
+self.group = user => {
+  // root bypass
+  if (user.username === 'root') return 'superadmin'
+  for (const key of self.keys) {
+    if (user.permission === self.permissions[key]) {
+      return key
+    }
+  }
+  return null
+}
 
 // returns true if user is in the group OR higher
 self.is = (user, group) => {
